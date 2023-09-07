16cc411e462255c01f7f7e5d079e27028e44811d
Indo Krish <55687559+Indok93@users.noreply.github.com>
2023-09-07 09:59:34 +0200

    done

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,5 +1,5 @@
 // serch mode to determine serch button clicked
-let serchMode = "none";
+let searchMode = "none";
 
 // dom elements for function --------------------------------------------------------
 
@@ -9,33 +9,131 @@ const myfirstLetterInput = document.getElementById("firstLetterInput");
 const myfirstLetterSearchButton = document.getElementById("firstLetterSearch");
 
 myfirstLetterSearchButton.addEventListener("click", () => {
-  serchMode = "firstLetterSearch";
-  console.info(myfirstLetterInput.value);
+  searchMode = "firstLetterSearch";
+  // console.info(myfirstLetterInput.value);
+  myReasultView()
 });
 
 const myNameInput = document.getElementById("nameInput");
 const myNameSearchButton = document.getElementById("nameSearch");
 
 myNameSearchButton.addEventListener("click", () => {
-  serchMode = "nameSearch";
-  console.info(myNameInput.value);
-  getRecipiesByName(myNameInput.value);
+  searchMode = "nameSearch";
+  // console.info(myNameInput.value);
+  myReasultView();
 });
 
 const myIdInput = document.getElementById("idInput");
 const myIdSearchButton = document.getElementById("idSearch");
 
 myIdSearchButton.addEventListener("click", () => {
-  serchMode = "idSearch";
-  console.info(myIdInput.value);
+  searchMode = "idSearch";
+  // console.info(myIdInput.value);
+  myReasultView();
 });
 
 //-------------------------------------------------------------------------------------
 
 // fetch functions --------------------------------------------------------------------
 // your code goes here
+function myReasultView(myData) {
+  switch (searchMode) {
+    case 'firstLetterSearch':
+      console.log(myData);
+      getRecipiesByLetter(myfirstLetterInput.value);
+      break;
+
+    case 'nameSearch':
+      console.log(myData);
+      getRecipiesByName(myNameInput.value);
+      break;
+
+    case 'idSearch':
+      console.log(myData);
+      getRecipiesById(myIdInput.value);
+      break;
+
+    default:
+      console.warn("ooops no data to show from setupResultView");
+      break;
+  }
+}
 
 
+
+function getRecipiesByLetter(myFirstLetter) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${myFirstLetter}`)
+
+  .then((response) => {
+    return response.json();
+  })
+
+  .then((data) => {
+    console.log(data.meals);
+
+    let myTextHTML = "";
+
+      data.meals.map((myMeal) => {
+        myTextHTML += myMeal.strMeal + ", ";
+      });
+
+      myResultElement.textContent = myTextHTML;
+  })
+
+  .catch((error) => {
+    console.error(error);
+  })
+}
+
+function getRecipiesByName(myFirstName) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${myFirstName}`)
+
+  .then((response) => {
+    return response.json();
+  })
+
+  .then((data) => {
+    console.log(data.meals);
+
+    let myTextHTML = "";
+
+      data.meals.map((myMeal) => {
+        myTextHTML += myMeal.strMeal + ", ";
+      });
+
+      myResultElement.textContent = myTextHTML;
+
+  })
+
+  .catch((error) => {
+    console.error(error);
+  })
+}
+
+function getRecipiesById(myFirstId) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${myFirstId}`)
+
+  .then((response) => {
+    return response.json();
+  })
+
+  .then((data) => {
+    console.log(data.meals);
+
+    let myTextHTML = "";
+
+      data.meals.map((myMeal) => {
+        myTextHTML += myMeal.strMeal + ", ";
+      });
+
+      myResultElement.textContent = myTextHTML;
+
+  })
+
+  .catch((error) => {
+    console.error(error);
+  })
+}
 
 
 
@@ -45,7 +143,7 @@ myIdSearchButton.addEventListener("click", () => {
 // view code --------------------------------------------------------------------------
 
 function setupResultView(myData) {
-  switch (serchMode) {
+  switch (searchMode) {
     case "firstLetterSearch":
       console.log(myData);
       // do view stuff with the data here
